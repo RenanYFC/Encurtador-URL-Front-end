e0f5ff38cf152bcff847a8b27cc837d13ba27239
Renan Yamamoto <renanyfc07@gmail.com>
2026-02-20 22:17:35 -0300

    NavBar
    Criação da NavBar e importação da fonte IstokWeb

--- encurtador-url/src/App.tsx
+++ encurtador-url/src/App.tsx
@@ -7,28 +7,16 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <nav className='flex justify-between items-center p-5'>
+        <h1 className='text-[var(--primary)] text-4xl font-bold'>Encurtador de Links</h1>
+        <ul className='flex gap-7'>
+          <li>Regras de Negócio</li>
+          <li>Repositório</li>
+          <li>GitHub</li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
