60e4b8f9957e7b021c3f4d05479015c5c25e028d
Renan Yamamoto <renanyfc07@gmail.com>
2026-02-25 23:40:02 -0300

    Requisição POST à API

--- encurtador-url/src/App.tsx
+++ encurtador-url/src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import {Button, TextField, Switch, InputRadio} from './components'
+
 function App() {
   const [facil_ler, set_facil_ler] = useState(false);
+  const [link, setLink] = useState(null);
   return (
     <div className='min-h-screen flex flex-col'>
       <nav className='flex justify-between items-center p-5'>
@@ -15,6 +17,13 @@ function App() {
       </nav>
       <main className='w-full flex-1 flex items-center justify-center flex-col gap-10'>
           <TextField className="w-3/5">Digite seu link longo</TextField>
+          {
+            link &&
+            <div>
+              <p><a href={"http://localhost:5173/"+link}>{"http://localhost:5173/"+link}</a></p>
+            </div>
+          }
+
           {!facil_ler&&
           <div>
             <InputRadio checked={true} name='qtdd_caracteres'>5 caracteres</InputRadio>
@@ -27,11 +36,33 @@ function App() {
             <Switch onChange={()=>{set_facil_ler(!facil_ler); console.log(facil_ler)}}>Fácil de ler</Switch>
           </div>
 
-          <Button className="w-2/5">Gerar</Button>
+          <Button className="w-2/5" onClick={()=>{HandleSendButton(setLink)}}>Gerar</Button>
       </main>
     </div>
   )
 }
 
+const HandleSendButton = (setLink: React.Dispatch<React.SetStateAction<null>>) => {
+  console.log("clicado");
+  fetch("http://localhost:3000/urls", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+      "Accept": "application/json",
+      "key": "###"
+    }
+  })
+  .then((res)=>{
+    console.log(res);
+    return res.json();
+  })
+  .then((json)=>{
+    setLink(json.cod_url);
+    console.log(json);
+  })
+  .catch((err)=>{
+    console.log("erro: ",err)
+  })
+}
 
 export default App
